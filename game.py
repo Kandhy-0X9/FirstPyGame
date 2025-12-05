080d57de890a503302d6b3a9a8467cea001c0793
Kandhy-0X9 <kashengkandhy@gmail.com>
2025-12-05 10:56:32 -0500

    finsih

--- game.py
+++ game.py
@@ -1,10 +1,13 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((1280, 640))
-
+screen = pygame.display.set_mode((640, 640))
+potato = pygame.image.load('potato.png').convert()
 running = True
+x = 0
 while running:
+    screen.blit(potato, (x, 30))
+    x +=1
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
